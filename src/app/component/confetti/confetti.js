ec29e178ef5035f11070fb6f5584ffd0704aa126
rakategar <tegarrakaw@gmail.com>
2024-06-28 17:11:14 +0700

    modified:   package-lock.json 	modified:   package.json 	new file:   public/atas.png 	new file:   public/bawah.png 	new file:   public/gambar.jpeg 	new file:   src/app/component/Navbar/Navbar.jsx 	new file:   src/app/component/confetti/confetti.js 	modified:   src/app/globals.css 	deleted:    src/app/page.js 	new file:   src/app/page.jsx 	modified:   tailwind.config.js

---
+++ src/app/component/confetti/confetti.js
@@ -0,0 +1,191 @@
+// confetti.js
+export function startConfetti() {
+  "use strict";
+
+  var random = Math.random,
+    cos = Math.cos,
+    sin = Math.sin,
+    PI = Math.PI,
+    PI2 = PI * 2,
+    timer = undefined,
+    frame = undefined,
+    confetti = [];
+
+  var particles = 10,
+    spread = 40,
+    sizeMin = 3,
+    sizeMax = 12 - sizeMin,
+    eccentricity = 10,
+    deviation = 100,
+    dxThetaMin = -0.1,
+    dxThetaMax = -dxThetaMin - dxThetaMin,
+    dyMin = 0.13,
+    dyMax = 0.18,
+    dThetaMin = 0.4,
+    dThetaMax = 0.7 - dThetaMin;
+
+  var colors = ["❤️", "💛", "💚", "💙", "💜", "🧡"];
+
+  var container = document.createElement("div");
+  container.style.position = "fixed";
+  container.style.top = "0";
+  container.style.left = "0";
+  container.style.width = "100%";
+  container.style.height = "0";
+  container.style.overflow = "visible";
+  container.style.zIndex = "9999";
+
+  function Confetto() {
+    this.frame = 0;
+    this.outer = document.createElement("div");
+    this.inner = document.createElement("div");
+    this.inner.textContent = colors[Math.floor(random() * colors.length)];
+    this.outer.appendChild(this.inner);
+
+    var outerStyle = this.outer.style,
+      innerStyle = this.inner.style;
+    outerStyle.position = "absolute";
+    outerStyle.width = sizeMin + sizeMax * random() + "px";
+    outerStyle.height = sizeMin + sizeMax * random() + "px";
+    innerStyle.width = "100%";
+    innerStyle.height = "100%";
+    innerStyle.fontSize = "24px";
+
+    outerStyle.perspective = "50px";
+    outerStyle.transform = "rotate(" + 360 * random() + "deg)";
+    this.axis =
+      "rotate3D(" + cos(360 * random()) + "," + cos(360 * random()) + ",0,";
+    this.theta = 360 * random();
+    this.dTheta = dThetaMin + dThetaMax * random();
+    innerStyle.transform = this.axis + this.theta + "deg)";
+
+    this.x = window.innerWidth * random();
+    this.y = -deviation;
+    this.dx = sin(dxThetaMin + dxThetaMax * random());
+    this.dy = dyMin + dyMax * random();
+    outerStyle.left = this.x + "px";
+    outerStyle.top = this.y + "px";
+
+    this.splineX = createPoisson();
+    this.splineY = [];
+    for (var i = 1, l = this.splineX.length - 1; i < l; ++i)
+      this.splineY[i] = deviation * random();
+    this.splineY[0] = this.splineY[l] = deviation * random();
+
+    this.update = function (height, delta) {
+      this.frame += delta;
+      this.x += this.dx * delta;
+      this.y += this.dy * delta;
+      this.theta += this.dTheta * delta;
+
+      var phi = (this.frame % 7777) / 7777,
+        i = 0,
+        j = 1;
+      while (phi >= this.splineX[j]) i = j++;
+      var rho = interpolation(
+        this.splineY[i],
+        this.splineY[j],
+        (phi - this.splineX[i]) / (this.splineX[j] - this.splineX[i])
+      );
+      phi *= PI2;
+
+      outerStyle.left = this.x + rho * cos(phi) + "px";
+      outerStyle.top = this.y + rho * sin(phi) + "px";
+      innerStyle.transform = this.axis + this.theta + "deg)";
+      return this.y > height + deviation;
+    };
+  }
+
+  function createPoisson() {
+    var radius = 1 / eccentricity,
+      radius2 = radius + radius;
+    var domain = [radius, 1 - radius],
+      measure = 1 - radius2,
+      spline = [0, 1];
+    while (measure) {
+      var dart = measure * random(),
+        i,
+        l,
+        interval,
+        a,
+        b,
+        c,
+        d;
+
+      for (i = 0, l = domain.length, measure = 0; i < l; i += 2) {
+        a = domain[i];
+        b = domain[i + 1];
+        interval = b - a;
+        if (dart < measure + interval) {
+          spline.push((dart += a - measure));
+          break;
+        }
+        measure += interval;
+      }
+      c = dart - radius;
+      d = dart + radius;
+
+      for (i = domain.length - 1; i > 0; i -= 2) {
+        l = i - 1;
+        a = domain[l];
+        b = domain[i];
+        if (a >= c && a < d)
+          if (b > d) domain[l] = d;
+          else domain.splice(l, 2);
+        else if (a < c && b > c)
+          if (b <= d) domain[i] = c;
+          else domain.splice(i, 0, c, d);
+      }
+
+      for (i = 0, l = domain.length, measure = 0; i < l; i += 2)
+        measure += domain[i + 1] - domain[i];
+    }
+
+    return spline.sort();
+  }
+
+  function interpolation(a, b, t) {
+    return ((1 - cos(PI * t)) / 2) * (b - a) + a;
+  }
+
+  function poof() {
+    if (!frame) {
+      document.body.appendChild(container);
+
+      var count = 0;
+      (function addConfetto() {
+        var confetto = new Confetto();
+        confetti.push(confetto);
+        container.appendChild(confetto.outer);
+        timer = setTimeout(addConfetto, spread * random());
+      })(0);
+
+      setTimeout(() => {
+        clearTimeout(timer);
+        timer = undefined;
+      }, 3000);
+
+      var prev = undefined;
+      requestAnimationFrame(function loop(timestamp) {
+        var delta = prev ? timestamp - prev : 0;
+        prev = timestamp;
+        var height = window.innerHeight;
+
+        for (var i = confetti.length - 1; i >= 0; --i) {
+          if (confetti[i].update(height, delta)) {
+            container.removeChild(confetti[i].outer);
+            confetti.splice(i, 1);
+          }
+        }
+
+        if (timer || confetti.length)
+          return (frame = requestAnimationFrame(loop));
+
+        document.body.removeChild(container);
+        frame = undefined;
+      });
+    }
+  }
+
+  poof();
+}
---
+++ src/app/page.jsx
@@ -0,0 +1,83 @@
+"use client";
+
+import { useState } from 'react';
+import Image from "next/image";
+import Navbar from "./component/Navbar/Navbar";
+import { startConfetti } from './component/confetti/confetti';
+
+export default function Home() {
+  const [name1, setName1] = useState("");
+  const [name2, setName2] = useState("");
+  const [percentage, setPercentage] = useState(null);
+  const [calculated, setCalculated] = useState(false);
+
+  const handleCalculate = () => {
+    const randomPercentage = Math.floor(Math.random() * 61) + 40; // generates a random number between 40 and 100
+    setPercentage(randomPercentage);
+    startConfetti(); // Panggil fungsi startConfetti ketika tombol "Hitung" diklik
+    setCalculated(true);
+  };
+
+  const handleReset = () => {
+    setName1("");
+    setName2("");
+    setPercentage(null);
+    setCalculated(false);
+  };
+
+  return (
+    <div className="bg-red-250 flex-grow">
+      <Navbar />
+      <div className="flex justify-center items-center pt-10">
+        <div className="card bg-base-100 w-96 h-auto shadow-xl border-white border-">
+          <figure className="px-10 pt-10">
+            <img
+              src="gambar.jpeg"
+              alt="Shoes"
+              className="rounded-xl"
+            />
+          </figure>
+          <div className="card-body items-center text-center">
+            <h2 className="card-title">Berapa Persen Cintamu ?</h2>
+            <p>Masukkan Namamu dan Nama Pasanganmu</p>
+            {!calculated ? (
+              <div className='flex flex-row gap-2 items-center'>
+                <div className="mb-4 text-sm">
+                  <input
+                    type="text"
+                    placeholder="Namamu"
+                    className="input input-bordered w-40"
+                    value={name1}
+                    onChange={(e) => setName1(e.target.value)}
+                  />
+                </div>
+                <div>🩷</div>
+                <div className="mb-4 text-sm">
+                  <input
+                    type="text"
+                    placeholder="Nama Pasangan"
+                    className="input input-bordered w-40"
+                    value={name2}
+                    onChange={(e) => setName2(e.target.value)}
+                  />
+                </div>
+              </div>
+            ) : (
+              <div className="mt-4 gap-4 mb-2 flex flex-col">
+                <p className='text-2xl font-bold'>{name1} 🩷 {name2} </p>
+                <p className='text-7xl font-bold'>{percentage}%</p>
+              </div>
+            )}
+            <div className="card-actions">
+              {!calculated ? (
+                <button className="btn btn-primary text-white px-24" onClick={handleCalculate}>Hitung</button>
+              ) : (
+                <button className="btn btn-secondary text-white px-24" onClick={handleReset}>Cek Lagi</button>
+              )}
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
